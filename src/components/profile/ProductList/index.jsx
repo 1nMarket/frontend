7f9e3c87a6a3c35b4e3c5f86a3b63e45046751b2
wSeungMi <seungmi.dev@gmail.com>
2022-12-12 17:25:54 +0900

    [feat] 판매중인 상품 기능_사용자 정보 불러오기 #7

--- src/components/profile/ProductList/index.jsx
+++ src/components/profile/ProductList/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { axiosPrivate } from '../../../apis/axios';
 
 const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
-}
+  // params 객체로 받아옴
+  const { accountname } = useParams();
+  const [productList, setProductList] = useState([]);
 
-export default ProductList
+  useEffect(() => {
+    const getProductList = async () => {
+      const {
+        data: { product },
+      } = await axiosPrivate.get(`/product/${accountname}`);
+      setProductList(product);
+    };
+    getProductList();
+  }, []);
+
+  return (
+    <>
+      {productList.length &&
+        productList.map((product, i) => (
+          <p key={product.id}>{product.itemName}</p>
+        ))}
+    </>
+  );
+};
+
+export default ProductList;
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ProfileInfo from '../../components/profile/ProfileInfo';
+import ProductList from '../../components/profile/ProductList';
 
 const Profile = () => {
   const { accountname } = useParams();
 
-  return <ProfileInfo accountname={accountname} />;
+  return (
+    <>
+      <ProfileInfo accountname={accountname} />
+      <ProductList />
+    </>
+  );
 };
 
 export default Profile;
