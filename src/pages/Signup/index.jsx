4a4f95e8f25b4b76c3185eee7119756a9012a2ec
wSeungMi <seungmi.dev@gmail.com>
2022-12-15 13:44:17 +0900

    [feat] signup_input focus 자동으로 맞추기 #21

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
+import { useRef } from 'react';
+import { useEffect } from 'react';
+// import axios from 'axios';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const inputRef = useRef();
 
-  console.log(email, password);
+  // email 유효성 검사 체크
+  useEffect(() => {
+    // input focus
+    inputRef.current.focus();
+
+    // email 정규식
+    //   const regexEmail = '';
+  }, []);
 
   return (
     <form>
@@ -16,9 +27,10 @@ const Signup = () => {
         placeholder='이메일 주소를 입력해 주세요.'
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        ref={inputRef}
       />
+      <p>이미 가입된 이메일 주소입니다.</p>
       <br />
-
       <label htmlFor='pwd'>비밀번호</label>
       <br />
       <input
@@ -28,9 +40,11 @@ const Signup = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
+      <p>비밀번호는 6자 이상이여야 합니다.</p>
       <br />
-
-      <button>다음</button>
+      <button>다음</button> <br />
+      <br />
+      <span>입력된 값: {password}</span>
     </form>
   );
 };
