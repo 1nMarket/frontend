b1bc5378c722e4075ce89a3918a94e7912ef2830
oneny <gjwodnjs1996@gmail.com>
2022-12-16 12:51:44 +0900

    [feat] 이미지 슬라이드 기능 구현 #27

--- src/components/post/ImageSlide/index.jsx
+++ src/components/post/ImageSlide/index.jsx
@@ -1,9 +1,19 @@
+import React, { useRef, useState } from 'react';
 import * as S from './style';
 
 const ImageSlide = ({ images }) => {
+  const [selectBtn, setSelectBtn] = useState(0);
+  const slideRef = useRef();
+  const handleSlide = (i) => {
+    const { current: slideEl } = slideRef;
+    const { width } = slideEl.getBoundingClientRect();
+    slideEl.style.transform = `translateX(${(-width / images.length) * i}px)`;
+    setSelectBtn(i);
+  };
+
   return (
     <S.SlideContainer imgLength={images.length}>
-      <S.Slide imgLength={images.length}>
+      <S.Slide ref={slideRef} imgLength={images.length}>
         {images.map((image, i) => (
           <S.SlideItem key={i}>
             <S.PostImage src={image} alt='' />
@@ -11,7 +21,14 @@ const ImageSlide = ({ images }) => {
         ))}
       </S.Slide>
       <S.ButtonWrapper>
-        {images.length > 1 && images.map((_, i) => <S.SlideButton key={i} />)}
+        {images.length > 1 &&
+          images.map((_, i) => (
+            <S.SlideButton
+              style={{ backgroundColor: selectBtn === i && '#495573' }}
+              key={i}
+              onClick={() => handleSlide(i)}
+            />
+          ))}
       </S.ButtonWrapper>
     </S.SlideContainer>
   );
