46f8510016adf71f00613afa44954f6745f45ceb
oneny <gjwodnjs1996@gmail.com>
2022-12-17 16:09:00 +0900

    [feat] 업로드 버튼 기능 구현 #39

--- src/pages/PostUpload/index.jsx
+++ src/pages/PostUpload/index.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { axiosImgUpload } from '../../apis/axios';
 
 const PostUpload = () => {
   const [imgFiles, setImgFiles] = useState([]);
+  const fileRef = useRef();
 
   const handleImgUpload = async (e) => {
     if (imgFiles.length > 2) return window.alert('이미지는 세 개 이하로 업로드가 가능합니다!');
@@ -12,21 +13,23 @@ const PostUpload = () => {
       '/image/uploadfile',
       form
     );
-    
+
     setImgFiles((prev) => [...prev, `https://mandarin.api.weniv.co.kr/${data.filename}`]);
   };
-  console.log(imgFiles);
 
   return (
     <div>
+      <button onClick={() => fileRef.current.click()}>이미지 업로드</button>
       <label htmlFor='img' />
       <input
+        style={{ display: 'none' }}
+        ref={fileRef}
         id='img'
         type='file'
         accept='.jpg, .gif, .png, .jpeg, .bmp, .tif, .heic'
         onChange={handleImgUpload}
       />
-      {imgFiles.length && imgFiles.map((img, i) => (
+      {imgFiles?.map((img, i) => (
         <img key={i} src={img} alt='' />
       ))}
     </div>
