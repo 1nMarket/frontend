c8bc13c9e047b337f3c9dcbb89d598511f915791
angenehms <angenehms@gmail.com>
2022-12-19 10:06:56 +0900

    [feat] common_Navbar 네비바 아이콘 클릭시 해당 페이지 이동기능 구현 #40

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -6,35 +6,42 @@ import { ReactComponent as ChatIcon } from '../../../assets/icons/icon-message-c
 import { ReactComponent as PostIcon } from '../../../assets/icons/icon-edit.svg'
 import { ReactComponent as ProfileIcon } from "../../../assets/icons/icon-user.svg"
 
-
-
 const Navbar = () => {
+  const accountName = JSON.parse(localStorage.getItem('accountname'));
+
   return (
     <>
       <Outlet />
       <S.NavWrapper>
         <S.NavList>
-          <S.NavItem>
-            <HomeIcon/>
-            <S.NavIconText>홈</S.NavIconText>
-          </S.NavItem>
+          <li>
+            <S.StyledLink to='/home'>
+              <HomeIcon/>
+              <S.NavIconText>홈</S.NavIconText>
+            </S.StyledLink>
+          </li>
 
-          <S.NavItem>
-            <ChatIcon/>
-            <S.NavIconText>채팅</S.NavIconText>
-          </S.NavItem>
+          <li>
+            <S.StyledLink to='/chat'>
+              <ChatIcon/>
+              <S.NavIconText>채팅</S.NavIconText>
+            </S.StyledLink>
+          </li>
 
-          <S.NavItem>
-            <PostIcon/>
-            <S.NavIconText>게시글 작성</S.NavIconText>
-          </S.NavItem>
+          <li>
+            <S.StyledLink to='/post/upload'>
+              <PostIcon/>
+              <S.NavIconText>게시글 작성</S.NavIconText>
+            </S.StyledLink>
+          </li>
 
-          <S.NavItem>
-            <ProfileIcon/>
-            <S.NavIconText>프로필</S.NavIconText>
-          </S.NavItem>
+          <li>
+            <S.StyledLink to={`/profile/${accountName}`}>
+              <ProfileIcon/>
+              <S.NavIconText>프로필</S.NavIconText>
+            </S.StyledLink>
+          </li>
         </S.NavList>
-
       </S.NavWrapper>
     </>
   )
--- src/components/common/Navbar/style.js
+++ src/components/common/Navbar/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const NavWrapper = styled.nav`
   width: inherit;
@@ -7,23 +8,27 @@ export const NavWrapper = styled.nav`
   height: 60px;
   padding: 10px 30px 10px 35px;
   border-top: 1px solid ${({ theme }) => theme.palette.border};
+  background-color: ${({ theme }) => theme.palette.white};
 `;
 
 export const NavList = styled.ul`
-display: flex;
-justify-content: space-between;
-align-items: center;`;
-
-export const NavItem = styled.li`
-display: flex;
-flex-direction: column;
-align-items: center;
-row-gap: 4px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const NavIconText = styled.p`
-font-size: 10px;
-color: ${({theme}) => theme.palette.mediumGray}
+  font-size: 10px;
+  color: ${({theme}) => theme.palette.mediumGray}
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: ${({ theme }) => theme.palette.black};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 4px;
 `;
 
 
