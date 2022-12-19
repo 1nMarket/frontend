b05de482e406653d6bf73f4ae0457c73d03e8698
oneny <gjwodnjs1996@gmail.com>
2022-12-19 15:08:12 +0900

    [design] 신고하기 경고 모달 스타일 #46

--- src/components/alertModals/ProductRemoveModal/index.jsx
+++ src/components/alertModals/ProductRemoveModal/index.jsx
@@ -3,7 +3,7 @@ import AlertModalLayout from '../AlertModalLayout';
 
 const ProductRemoveModal = () => {
   return (
-    <AlertModalLayout comment='게시글을 삭제할까요?'>
+    <AlertModalLayout comment='상품을 삭제할까요?'>
       <li>취소</li>
       <li>삭제</li>
     </AlertModalLayout>
