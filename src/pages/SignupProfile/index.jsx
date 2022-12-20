c67d39ddc74c16bb8c028ce7b1b779b454292894
wSeungMi <seungmi.dev@gmail.com>
2022-12-20 10:40:18 +0900

    [feat] signup_계정 ID 유효성 체크 #44

--- src/pages/SignupProfile/index.jsx
+++ src/pages/SignupProfile/index.jsx
@@ -3,6 +3,8 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import { axiosImgUpload } from '../../apis/axios';
 
 const USERNAME_REGEX = /^[a-zA-Z0-9가-힣]{2,10}$/;
+// eslint-disable-next-line
+const ACCOUNTNAME_REGEX = /^(?=[a-z])[a-z0-9\.\_]{5,15}$/;
 
 const SignupProfile = () => {
   const { state } = useLocation();
@@ -11,9 +13,14 @@ const SignupProfile = () => {
     'https://mandarin.api.weniv.co.kr/1671431659709.png',
   );
   const [userName, setUserName] = useState('');
+  console.log('userName :' + !!userName);
   const [validUserName, setValidUserName] = useState('false');
   console.log(validUserName);
   const [errUserNameMsg, setErrUserNameMsg] = useState('');
+  const [accountName, setAccountName] = useState('');
+  const [validAccountName, setValidAccountName] = useState('false');
+  console.log(validAccountName);
+  const [errAccountNameMsg, setErrAccountNameMsg] = useState('');
 
   const handleImgUpload = async (e) => {
     let form = new FormData();
@@ -38,12 +45,27 @@ const SignupProfile = () => {
     const result = USERNAME_REGEX.test(userName);
     setValidUserName(result);
 
-    if (!userName || result) {
+    if (userName.length && !result) {
+      setErrUserNameMsg('2~10자의 한글,영어,숫자만 사용 가능합니다.');
+    } else {
       setErrUserNameMsg('');
-    } else if (userName.length && !result) {
-      setErrUserNameMsg('2자~10자 이내여야 합니다.');
     }
   }, [userName]);
+
+  // 계정 id 유효성 체크
+  useEffect(() => {
+    const result = ACCOUNTNAME_REGEX.test(accountName);
+    setValidAccountName(result);
+    console.log('accountName: ' + result);
+
+    if (accountName.length && !result) {
+      setErrAccountNameMsg(
+        '5~15자 이내의 영문 소문자, 숫자와 특수문자 마침표(.), 밑줄(_)만 사용 가능합니다.',
+      );
+    } else {
+      setErrAccountNameMsg('');
+    }
+  }, [accountName]);
 
   return (
     <>
@@ -64,27 +86,32 @@ const SignupProfile = () => {
             />
           </div>
           <div>
-            <label htmlFor='userName'>사용자 이름</label> <br />
+            <label htmlFor='userName'>사용자 이름(필수)</label> <br />
             <input
               id='userName'
               placeholder='2~10자 이내여야 합니다.'
+              maxLength='10'
               value={userName}
               onChange={(e) => setUserName(e.target.value)}
             />
             {errUserNameMsg && <p>{errUserNameMsg}</p>}
           </div>
           <div>
-            <label htmlFor='accountId'>계정 ID</label> <br />
+            <label htmlFor='accountName'>계정 ID(필수)</label> <br />
             <input
-              id='accountId'
-              placeholder='영문, 숫자, 특수문자(.),(_)만 사용 가능합니다.'
+              id='accountName'
+              placeholder='5~15자 이내의 영문 소문자, 숫자와 특수문자 마침표(.), 밑줄(_)만 사용 가능합니다.'
+              maxLength='15'
+              value={accountName}
+              onChange={(e) => setAccountName(e.target.value)}
             />
+            {errAccountNameMsg && <p>{errAccountNameMsg}</p>}
           </div>
           <div>
             <label htmlFor='introduce'>소개</label> <br />
             <input id='introduce' placeholder='자신에 대해 소개해 주세요!' />
           </div>
-          <button>1n마켓 시작하기</button>
+          <button disabled>1n마켓 시작하기</button>
         </form>
       </section>
     </>
