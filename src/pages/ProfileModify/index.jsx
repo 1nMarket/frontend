a6ff4c34975dcb1366e3e7917380f68a6df691ee
wSeungMi <seungmi.dev@gmail.com>
2022-12-26 16:22:39 +0900

    [feat] profileModify_프로필 수정 페이지 헤더 연결 #67

--- src/pages/ProfileModify/index.jsx
+++ src/pages/ProfileModify/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import SaveHeader from '../../components/common/Header/SaveHeader';
 
 const ProfileModify = () => {
-  return <div>프로필 수정페이지</div>;
+  return (
+    <>
+      <SaveHeader />
+      <div>프로필 수정페이지</div>
+    </>
+  );
 };
 
 export default ProfileModify;
