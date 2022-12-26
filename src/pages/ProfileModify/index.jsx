beb5544756d46e425b973f1496439632d4fa08e2
wSeungMi <seungmi.dev@gmail.com>
2022-12-26 20:23:17 +0900

    [feat] profileModify_userProfileForm 컴포넌트 불러오기 #67

--- src/pages/ProfileModify/index.jsx
+++ src/pages/ProfileModify/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SaveHeader from '../../components/common/Header/SaveHeader';
+import UserProfileForm from '../../components/user/UserProfileForm';
 
 const ProfileModify = () => {
+  const [profileImg, setProfileImg] = useState('');
+  const [username, setUsername] = useState('');
+  const [accountname, setAccountname] = useState('');
+  const [intro, setIntro] = useState('');
+  const [canSave, setCanSave] = useState(false);
+
   return (
     <>
       <SaveHeader />
-      <div>프로필 수정페이지</div>
+      <UserProfileForm
+        profileImg={profileImg}
+        setProfileImg={setProfileImg}
+        username={username}
+        setUsername={setUsername}
+        accountname={accountname}
+        setAccountname={setAccountname}
+        intro={intro}
+        setIntro={setIntro}
+        canSave={canSave}
+        setCanSave={setCanSave}
+      />
     </>
   );
 };
