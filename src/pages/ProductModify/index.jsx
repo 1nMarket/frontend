44141372fd5d94b676c7a80a7894687807cac43a
angenehms <angenehms@gmail.com>
2022-12-27 03:44:16 +0900

    [feat] productmodify_상품 수정사항 업로드 기능 구현 #71

--- src/pages/ProductModify/index.jsx
+++ src/pages/ProductModify/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useState } from 'react';
+import { axiosPrivate } from '../../apis/axios';
 import SaveHeader from '../../components/common/Header/SaveHeader';
 import ProductForm from '../../components/post/ProductForm';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const ProductModify = () => {
     const [imgFiles, setImgFiles] = useState('');
@@ -9,11 +11,32 @@ const ProductModify = () => {
     const [productLink, setProductLink] = useState('');
     const [productPrice, setProductPrice] = useState('');
     const [canSave, setCansave] = useState(false);
-    console.log(canSave);
+
+    const navigate = useNavigate();
+    const { postId } = useParams();
+    const accountName = JSON.parse(localStorage.getItem('accountname'));
+
+    const handleProductUpload = async (e) => {
+        console.log(productName, productPrice, productLink, imgFiles);
+        e.preventDefault();
+        if (!canSave) return;
+        await axiosPrivate.put(
+          `/product/${postId}`,
+          {
+            product: {
+              itemName: productName,
+              price: Number(productPrice.replaceAll(",", "")),
+              link: productLink,
+              itemImage: imgFiles
+            },
+          },
+        )
+        navigate(`/profile/${accountName}`);
+      };
 
   return (
     <>
-        <SaveHeader canSave={canSave}/> 
+        <SaveHeader canSave={canSave} handleProductUpload={handleProductUpload}/> 
         <ProductForm
           imgFiles={imgFiles}
           setImgFiles={setImgFiles}
