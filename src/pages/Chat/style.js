ad098423b848f02af143180047c962fbad9f15d8
angenehms <angenehms@gmail.com>
2022-12-27 15:53:53 +0900

    [design] chat_채팅 페이지 메인 태그 전체적인 레이아웃(컨텐츠 요소와의 간격) 맞추기 #74

--- src/pages/Chat/style.js
+++ src/pages/Chat/style.js
@@ -3,29 +3,25 @@ import BasicProfile from '../../assets/icons/basic-profile.svg';
 import NewChat from '../../assets/icons/NewChat.svg';
 
 export const Content = styled.main`
-    padding-top: 48px;
+    padding: 72px 16px 0px;
 `;
 
-export const ChatList = styled.ul`
+export const ChatList = styled.ul``;
 
-`;
-
-export const ChatItem = styled.li`
-
-`;
+export const ChatItem = styled.li``;
 
 export const BasicProfileIcon = styled.img.attrs({
     src: BasicProfile,
     alt: '기본 프로필 아이콘',
   })``;
 
-  export const NewChatMark = styled.img.attrs({
+export const NewChatMark = styled.img.attrs({
     src: NewChat,
     alt: '새로운 메세지 표시',
   })``;
 
-  export const ChatUserName = styled.p``;
+export const ChatUserName = styled.p``;
 
-  export const ChatContent = styled.p``;
+export const ChatContent = styled.p``;
 
-  export const ChatDate = styled.p``;
+export const ChatDate = styled.p``;
