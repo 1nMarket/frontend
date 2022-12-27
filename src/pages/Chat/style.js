1d02592bc051d77657b637e2b5e16865b9582889
angenehms <angenehms@gmail.com>
2022-12-27 16:17:35 +0900

    [design] chat_채팅 페이지 글자크기, 색상, 아이콘 크기 스타일링 #74

--- src/pages/Chat/style.js
+++ src/pages/Chat/style.js
@@ -13,15 +13,30 @@ export const ChatItem = styled.li``;
 export const BasicProfileIcon = styled.img.attrs({
     src: BasicProfile,
     alt: '기본 프로필 아이콘',
-  })``;
+  })`
+    width: 42px;
+    height: 42px;
+`;
 
 export const NewChatMark = styled.img.attrs({
     src: NewChat,
     alt: '새로운 메세지 표시',
-  })``;
+  })`
+    width: 12px;
+    height: 12px;  
+`;
 
-export const ChatUserName = styled.p``;
+export const ChatUserName = styled.p`
+    font-size: 14px;
+    color: ${({ theme }) => theme.palette.black};
+`;
 
-export const ChatContent = styled.p``;
+export const ChatContent = styled.p`
+    font-size: 12px;
+    color: ${({ theme }) => theme.palette.mediumGray};
+`;
 
-export const ChatDate = styled.p``;
+export const ChatDate = styled.p`
+    font-size: 10px;
+    color: ${({ theme }) => theme.palette.border};
+`;
