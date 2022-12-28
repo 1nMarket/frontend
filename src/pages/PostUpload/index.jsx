0589a18610cb719bfd6feb659ee9c14126d2e7ae
angenehms <angenehms@gmail.com>
2022-12-28 20:37:39 +0900

    [refactor] postmodify_PostUpload 파일의 form 부분을 다른 컴포넌트(PostForm)로 분리 #93

--- src/pages/PostUpload/index.jsx
+++ src/pages/PostUpload/index.jsx
@@ -1,38 +1,15 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { axiosImgUpload, axiosPrivate } from '../../apis/axios';
+import { axiosPrivate } from '../../apis/axios';
 import UploadHeader from '../../components/common/Header/UploadHeader';
-import { ReactComponent as UploadIcon } from '../../assets/icons/icon-upload.svg';
-import * as S from './style';
-import { useRef } from 'react';
+import PostForm from '../../components/post/PostForm';
 
 const PostUpload = () => {
   const navigate = useNavigate();
   const [content, setContent] = useState('');
   const [imgFiles, setImgFiles] = useState([]);
-  const textRef = useRef();
 
   const canSave = !!imgFiles.length || !!content;
-
-  const handleImgUpload = async (e) => {
-    e.preventDefault();
-    if (imgFiles.length > 2)
-      return window.alert('이미지는 세 개 이하로 업로드가 가능합니다!');
-    const form = new FormData();
-    form.append('image', e.target.files[0]);
-    const { data } = await axiosImgUpload.post('/image/uploadfile', form);
-
-    setImgFiles((prev) => [
-      ...prev,
-      `https://mandarin.api.weniv.co.kr/${data.filename}`,
-    ]);
-  };
-
-  const autoResizeTextarea = (e) => {
-    textRef.current.style.height = 'auto';
-    let height = textRef.current.scrollHeight; // 높이
-    textRef.current.style.height = `${height + 16}px`;
-  };
 
   const handlePostUpload = async (e) => {
     console.log(content, imgFiles);
@@ -50,49 +27,15 @@ const PostUpload = () => {
     navigate('/home');
   };
 
-  const handleRemoveImg = (idx) => {
-    setImgFiles((prev) => prev.filter((_, i) => idx !== i));
-  }
-
   return (
     <>
       <UploadHeader canSave={canSave} handlePostUpload={handlePostUpload} />
-      <S.Conatiner>
-        <S.ProfileImg
-          src={JSON.parse(localStorage.getItem('profile-img'))}
-          alt='사용자 이미지'
+      <PostForm 
+        content={content} 
+        setcontent={setContent} 
+        imgFiles={imgFiles}
+        setImgFiles={setImgFiles}
         />
-        <S.PostForm>
-          <S.PostComment
-            ref={textRef}
-            value={content}
-            placeholder='게시글 입력하기...'
-            onChange={(e) => setContent(e.target.value)}
-            onKeyDown={autoResizeTextarea}
-            onKeyUp={autoResizeTextarea}
-          />
-          <S.ImgLabel htmlFor='img'>
-            <UploadIcon />
-          </S.ImgLabel>
-          <input
-            style={{ display: 'none' }}
-            id='img'
-            type='file'
-            accept='.jpg, .gif, .png, .jpeg, .bmp, .tif, .heic'
-            onChange={handleImgUpload}
-          />
-          <S.ImgList>
-            {imgFiles?.map((img, i) => (
-              <S.ImgItem key={img.slice(10)} >
-                <S.PostImg src={img} alt='' />
-                <S.RemoveButton type='button' onClick={() => handleRemoveImg(i)}>
-                  <span className='ir'>이미지 삭제</span>
-                </S.RemoveButton>
-              </S.ImgItem>
-            ))}
-          </S.ImgList>
-        </S.PostForm>
-      </S.Conatiner>
     </>
   );
 };
